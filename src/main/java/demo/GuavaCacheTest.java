1a4b36eaa3743289ba273a74386abdb005657b8b
lytles@takashi <srl.source@nqzero.com>
2018-08-19 17:26:25 -0400

    nqzero - use lambdas to simplify demo

--- src/main/java/demo/GuavaCacheTest.java
+++ src/main/java/demo/GuavaCacheTest.java
@@ -31,81 +31,36 @@ public class GuavaCacheTest {
 
 
     public static final Integer dummy = new Integer(-42883);
+    Random random = new Random();
 
     class KilimCacheLoader extends CacheLoader<String, Integer> {
-
-
-        /**
-         * guava doesn't support an async load, so just return a dummy value
-         *
-         * @param key
-         * @return
-         * @throws Exception
-         * @throws Pausable
-         */
-        @Override
         public Integer load(String key) {
             return dummy;
         }
 
-        /**
-         * this method will reload the cache asynchronously , it should not block the main stream either .
-         * @param key
-         * @param oldValue
-         * @return
-         * @throws Exception
-         * @throws Pausable
-         */
-        @Override
         public ListenableFuture reload(String key, Integer oldValue) {
-            LoadTask loadTask = new LoadTask();
-            loadTask.start();
-            return loadTask.future;
+            SettableFuture future = SettableFuture.create();
+            Task.fork(() -> {
+                Task.sleep(10);
+                future.set(random.nextInt(1000));
+            });
+            return future;
         }
 
     }
 
-    class LoadTask extends Task {
-        SettableFuture future = SettableFuture.create();
-        Random random = new Random();
-
-        @Override
-        public void execute() throws Pausable {
-            sleep(10);
-            future.set(random.nextInt(1000));
-        }
-    }
-
-    /**
-     * get a value from the cache, retrying until it loads
-     * @param key
-     * @return the value associated with the key
-     * @throws Pausable 
-     */
     public Integer get(String key) throws Pausable {
         Integer result = null;
         while (true) {
             try {
                 result = loadingCache.get(key);
+                if (result==dummy)
+                    loadingCache.refresh(key);
+                else
+                    return result;
             }
             catch (ExecutionException ex) {}
-            if (result==dummy)
-                loadingCache.refresh(key);
-            else if (result==null)
-                System.out.println("retry");
-            else
-                return result;
             Task.sleep(100);
-        }
-    }
-    
-    class QuizTask extends Task {
-        public void execute() throws Pausable {
-            while (true) {
-                Integer val = get("any_key");
-                System.out.println(val);
-                Task.sleep(100);
-            }
         }
     }
     
@@ -113,9 +68,15 @@ public class GuavaCacheTest {
     public static void main(String[] args) throws Exception {
         if (kilim.tools.Kilim.trampoline(false,args))
             return;
-        GuavaCacheTest guavaCacheTest = new GuavaCacheTest();
+        GuavaCacheTest cache = new GuavaCacheTest();
 
-        guavaCacheTest.new QuizTask().start();
+        Task.fork(() -> {
+            while (true) {
+                System.out.println(cache.get("any_key"));
+                Task.sleep(100);
+            }
+
+        });
 
     }
 
