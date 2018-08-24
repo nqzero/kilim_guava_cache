137af454b4f935bf8608ae0c4532c24ff832832c
lytles@takashi <srl.source@nqzero.com>
2018-08-24 19:04:23 -0400

    nqzero - stress the cache with many tasks in the demo

--- src/main/java/demo/GuavaCacheDemo.java
+++ src/main/java/demo/GuavaCacheDemo.java
@@ -9,11 +9,9 @@ import java.util.concurrent.TimeUnit;
 import static demo.KilimCacheLoader.getCache;
 
 /**
- * example usage of the kilim-guava-cache-integration.
+ * stress test of the kilim-guava-cache-integration.
  * the first load() returns a dummy value which triggers an asynchronous reload.
  * all access is performed using the pausable getCache
- *
- * Created by adamshuang on 2018/8/9.
  */
 public class GuavaCacheDemo {
     
@@ -22,26 +20,44 @@ public class GuavaCacheDemo {
             return;
 
         Random random = new Random();
+        int numTasks = 1000;
+        int maxIters = 100;
+        int maxKey = 1100;
+        int maxVal = 1000;
+        int maxDelay = 1000;
+        int maxSize = 1000;
+        int refresh = 10;
+        int maxWait = 1000;
+        int retry = 200;
 
-        LoadingCache<String,Integer> cache = CacheBuilder.newBuilder()
-                .refreshAfterWrite(1,TimeUnit.SECONDS)
+        LoadingCache<Integer,Integer> cache = CacheBuilder.newBuilder()
+                .refreshAfterWrite(refresh,TimeUnit.SECONDS)
+                .maximumSize(maxSize)
                 .build(new KilimCacheLoader(
                         future -> {
-                            Task.sleep(10);
-                            future.set(random.nextInt(1000));
+                            Task.sleep(random.nextInt(maxDelay));
+                            future.set(random.nextInt(maxVal));
                         }
                 ));
         
         
-        
-        Task.fork(() -> {
-            while (true) {
-                int val = getCache(cache,"any_key",50);
-                System.out.println(val);
-                Task.sleep(100);
-            }
 
-        });
+
+        for (int jj=0; jj < numTasks; jj++) {
+            int ktask = jj;
+            Task.fork(() -> {
+                while (true) {
+                    int numIters = random.nextInt(maxIters);
+                    for (int ii=0,sum=0; ii <= numIters; ii++) {
+                        Task.sleep(random.nextInt(maxWait));
+                        int key = random.nextInt(maxKey);
+                        sum += getCache(cache,key,retry);
+                        if (ii==numIters)
+                            System.out.format("cache: %4d -> %8d\n",ktask,sum);
+                    }
+                }
+            });
+        }
 
     }
 
